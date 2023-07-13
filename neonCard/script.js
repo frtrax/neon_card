4da1d62017615b1c618b1fdf10714a996e5b0147
frtrax <83843025+frtrax@users.noreply.github.com>
2023-07-13 17:31:29 +0200

    Add files via upload

---
+++ neonCard/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', function() {
+    var textElement = document.querySelector('.conseil span');
+    setTimeout(function() {
+      textElement.classList.add('show');
+    }, 600); // 3000 milliseconds = 3 seconds
+  });
+
+
+window.addEventListener('load', function() {
+var textElement = document.querySelector('.conseil2 span');
+setTimeout(function() {
+    textElement.classList.add('show');
+}, 600); // 3000 milliseconds = 3 seconds
+});
